8f499fa706f908797ef493ed742e379e77871eff
LuisAlvarado-19 <luafer95@gmail.com>
2021-03-04 20:22:44 +0000

    nuevo repositorio

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 
-let pronoun =["the","our"];
-let adj = ["great","big"];
-let noun = ["jogger","raccon","limon","cabrones"];
-let ext = [".com", ".net", ".us", ".io", ".es"];
-
-let domaingGenarator = () => {
-
-     let pronoun1 = pronoun[Math.floor((Math.random() * pronoun.length) )] ;
-     let adj1 = adj[Math.floor(Math.random() * adj.length)] ;
-     let noun2 = noun[Math.floor(Math.random() * noun.length)] ;
-     let ext2 = ext[Math.floor(Math.random() * ext.length)];
-
-     console.log(pronoun1 + adj1 + noun2 + ext2);    
-     
+//onload is the first event that happens in the browser*//
+window.onload = function() {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-domain").innerHTML = domaingGenarator();
+  });
+  console.log("Hello Rigo from the console! "); //genera la nueva excusa en cada refresh*//
 };
-domaingGenarator();
+let domaingGenarator = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "raccon", "limon", "apple"];
+  let ext = [".com", ".net", ".us", ".io", ".es"];
+
+  //usamos el math.floor para que no me genere numeros random con decimales*//
+  let pronoun1 = [Math.floor(Math.random() * pronoun.length)];
+  let adj1 = [Math.floor(Math.random() * adj.length)];
+  let noun1 = [Math.floor(Math.random() * noun.length)];
+  let ext1 = [Math.floor(Math.random() * ext.length)];
+
+  return (
+    pronoun[pronoun1] + adj[adj1] + noun[noun1] +  ext[ext1]
+  );
+};
+// domaingGenarator();
 
  
